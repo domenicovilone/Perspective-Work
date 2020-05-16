d2cd02a90e93bd02d33882a6e8dadc6af6d6c33a
domenico vilone <domenicovilone@gmail.com>
2020-05-16 13:57:46 -0400

    nth commit

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ var fs = require('fs');
 // for CSS, images, fonts, scripts
 app.use(express.static(path.join(__dirname)));
 app.use("/styles", express.static(__dirname + '/styles'));
-//app.use("/images", express.static(__dirname + '/images'));
+app.use("/images", express.static(__dirname + '/images'));
 app.use("/styles/fonts", express.static(__dirname + '/styles/fonts'));
 app.use("/scripts", express.static(__dirname + '/scripts'));
 
 // for HTML
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  res.sendFile(path.join(`${__dirname}/views/index.html`));
 });
+app.get('/questionnaire', function (req, res) {
+  res.sendFile(path.join(`${__dirname}/views/questionnaire.html`));
+});
+
+
 
 // for PORT
 app.listen(process.env.PORT || 2000);
