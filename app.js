b277345ded93b2cc6cd2cbec4538fd8c678d317a
domenico vilone <domenicovilone@gmail.com>
2020-09-01 10:33:02 -0400

    1

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var fs = require('fs');
 
-// for CSS, images, fonts, scripts
-app.use(express.static(__dirname, { 
+app.use(express.static(__dirname + '/views', { 
     extensions: ['html']
 }));
 app.use("/styles", express.static(__dirname + '/styles'));
@@ -12,22 +10,20 @@ app.use("/images", express.static(__dirname + '/images'));
 app.use("/styles/fonts", express.static(__dirname + '/styles/fonts'));
 app.use("/scripts", express.static(__dirname + '/scripts'));
 
-// for HTML
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(`${__dirname}/views/index.html`));
-// });
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(`${__dirname}/views/worksonperspective.html`));
-// });
 app.get('/', function (req, res) {
   res.sendFile(path.join(`${__dirname}/views/index.html`));
 });
-app.get('/views/book/', function (req, res) {
-    res.sendFile(path.join(`${__dirname}/views/worksonperspective`));
-  });
+app.get('/book', function (req, res) {
+    res.sendFile(path.join(`${__dirname}/views/book/worksonperspective.html`));
+});
+app.get('/book/#intro', function (req, res) {
+  res.sendFile(path.join(`${__dirname}/views/book/worksonperspective.html#intro`));
+});
+app.get('/book/#q-en', function (req, res) {
+  res.sendFile(path.join(`${__dirname}/views/book/worksonperspective.html#q-en`));
+});
+app.get('/book/#q-fr', function (req, res) {
+  res.sendFile(path.join(`${__dirname}/views/book/worksonperspective.html#q-fr`));
+});
 
-
-
-
-// for PORT
 app.listen(process.env.PORT || 2000);
--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,65 @@
-// Element to modify
-var LandingPageWrapper = document.querySelector('div.landing-page');
+// Elements to modify
+var mainPage = document.querySelector('div.main-wrapper');
+var book1 = document.querySelector('.book-1-wrapper-hide');
+var howToPublish = document.querySelector('div.how-it-works-wrapper');
 
 // Trigger elements
-var LandingPageBtn = document.querySelectorAll('div.content-2 div.services-wrapper button');
+var lpImage = document.querySelector('div.image-wrapper .img-sub-wrapper img');
+var moreBtn = document.querySelector('.more-btn-1');
+var backHome = document.querySelector('#back-homepage');
+var viewLib = document.querySelector('#view-lib');
+var learnToBtn = document.querySelector('#learnTo');
+var gotItBtn = document.querySelectorAll('#got-it-btn');
 
-
-// States
-var isOnMain = true;
-
-// Toggle landing page wrapper
-LandingPageBtn.forEach( (el,i) => el.addEventListener("click", function(){
-	toggleLandingPageWrapper(i);
-}));
-// Visit Book 1
-viewBook1.addEventListener("click", function(){
-    isOnMain = false;
-    revealBook1();
+// Show book1
+lpImage.addEventListener("click", function(){
+    showBook1();
 });
+moreBtn.addEventListener("click", function(){
+    showBook1();
+});
+// Show homeage
+backHome.addEventListener("click", function(){
+    showHome();
+});
+viewLib.addEventListener("click", function(){
+    showHome();
+});
+// Show How to publish page
+learnToBtn.addEventListener("click", function(){
+    showHowToPublish();
+});
+// Hide To to publish page
+gotItBtn.forEach( (el,i) => el.addEventListener("click", function(){
+	hideHowToPublish();
+}));
 
 
 
-// Utility function for toggles
-function isHidden(index, list, keyWord){
-    console.log(index);
-    return list.item(index).classList.contains(keyWord);
+
+function showBook1(){
+    book1.id = "";
+    setTimeout(hideMainPage, 2000);
+    book1.className = "book-1-wrapper pos-trs-r";
+}
+function showHome(){
+    mainPage.id = "";
+    book1.id = "hidden";
+    book1.className = "book-1-wrapper-hide pos-trs-r";
+}
+function showHowToPublish(){
+    howToPublish.id = "";
+    setTimeout(hideMainPage, 2000);
+}
+function hideHowToPublish(){
+    mainPage.id = "";
+    howToPublish.id = "hidden";
+}
+
+
+// Hide homepage
+function hideMainPage(){
+    mainPage.id = "hidden";
 }
 
 
