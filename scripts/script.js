dee059868391ca38046f4564323a4423726e9c65
domenico vilone <domenicovilone@gmail.com>
2020-09-04 17:00:53 -0400

    2

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@
 var mainPage = document.querySelector('div.main-wrapper');
 var book1 = document.querySelector('.book-1-wrapper-hide');
 var howToPublish = document.querySelector('div.how-it-works-wrapper');
+var tags1 = document.querySelector('.hashtag-wrapper-1');
+var libRow1 = document.querySelector('.lib-row-1');
 
 // Trigger elements
 var lpImage = document.querySelector('div.image-wrapper .img-sub-wrapper img');
@@ -10,6 +12,10 @@ var backHome = document.querySelector('#back-homepage');
 var viewLib = document.querySelector('#view-lib');
 var learnToBtn = document.querySelector('#learnTo');
 var gotItBtn = document.querySelectorAll('#got-it-btn');
+var tag1Btn = document.querySelector('.tag-1');
+
+// 
+var tagVisible = false;
 
 // Show book1
 lpImage.addEventListener("click", function(){
@@ -33,8 +39,14 @@ learnToBtn.addEventListener("click", function(){
 gotItBtn.forEach( (el,i) => el.addEventListener("click", function(){
 	hideHowToPublish();
 }));
-
-
+// Show/Hide book1 tags
+tag1Btn.addEventListener("click", function(){
+    if (tagVisible === true){
+        hideTags();
+    } else {
+        showTags();
+    }
+});
 
 
 function showBook1(){
@@ -55,7 +67,18 @@ function hideHowToPublish(){
     mainPage.id = "";
     howToPublish.id = "hidden";
 }
+function showTags(){
+    tags1.className = "hashtag-wrapper-1";
+    libRow1.className = "lib-row-1 lib-row-1-show-hashtag";
 
+    tagVisible = true; // set tags visible
+}
+function hideTags(){
+    tags1.className = "hidden hashtag-wrapper-1";
+    libRow1.className = "lib-row-1 lib-row-1-hide-hashtag";
+
+    tagVisible = false; // set tags invisible
+}
 
 // Hide homepage
 function hideMainPage(){
